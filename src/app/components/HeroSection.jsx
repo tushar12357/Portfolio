5cbd68f8833a18b83df892739da94a2fa618e48e
tushar12357 <110051562+tushar12357@users.noreply.github.com>
2024-05-17 19:11:44 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -35,11 +35,11 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/resumenew.pdf" target="_blank"
+              href="/Tushar Chaudhary_Guru Tegh Bahadur Institute of Technology.pdf" target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
+                Download Resume
               </span>
             </Link>
           </div>
