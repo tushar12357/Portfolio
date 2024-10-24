6189d9ace724c4b17fade6c15f3d7fc320bae0c0
Tushar Chaudhary <tusharcdry@gmail.com>
2024-10-24 22:39:02 +0530

    Update AboutSection.jsx

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -9,7 +9,7 @@ const TAB_DATA = [
     id: "skills",
     content: (
       <ul className="list-disc pl-2">
-        <li>Full Stack Web Development: HTML, CSS, JavaScript, ReactJS, Node.js, Express.js, MongoDB, Chart.js</li>
+        <li>Full Stack Web Development: HTML, CSS, JavaScript, ReactJS, Node.js, Express.js, MongoDB, Chart.js, RESTful APIs, React Native</li>
         <li>Programming Languages: C++, Python</li>
         <li>Database: MySQL</li>
       </ul>
@@ -64,12 +64,13 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Node.js, Express, MongoDB,
-            HTML, CSS, NextJS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+            I am a full stack developer with a passion for creating
+            interactive and responsive web and mobile applications. I have experience
+            working with JavaScript, React, React Native, Node.js, Express, MongoDB,
+            HTML, CSS, and Git, I am continuously expanding my skill set. 
+            A quick learner and a team player, I enjoy collaborating with other
+            to create innovative, user-friendly applications across web and 
+            mobile platforms.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
